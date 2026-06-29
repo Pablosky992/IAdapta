976f91584c4face6d0f7440f6422fa6ad45e8b91
Pablosky992 <pnarciso92@gmail.com>
2026-06-29 22:08:03 +0200

    Delete scratch directory

--- scratch/analyze_texture.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-
-// Since we know assets/iadapta_logo.png is a PNG, let's read some pixels.
-// To avoid importing complex libraries, let's look at the first few bytes or write a simple script.
-// Wait! Let's check if the file size is 3.8 MB. Yes! It is 3,848,988 bytes.
-// A 3.8 MB PNG file for 2158x1128 dimensions is very large, which usually indicates it has a photographic or textured background!
-// Let's write a simple script to copy a smaller version or check the unique colors.
-console.log("File size of assets/iadapta_logo.png:", fs.statSync("assets/iadapta_logo.png").size);
